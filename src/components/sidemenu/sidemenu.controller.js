40cce13ef0d19c82dd1dd8d8a89542fe3bcdb28e
Eduardo Koloma Jr <eduardo@kolomajr.com>
2018-02-09 12:59:35 +0200

    feat: added popover menu for settings option

--- src/components/sidemenu/sidemenu.controller.js
+++ src/components/sidemenu/sidemenu.controller.js
@@ -2,13 +2,23 @@
  * Created by Carl on 2017-06-08.
  */
 angular.module('app')
-    .controller('SidemenuCtrl', function ($scope, authService, $ionicSideMenuDelegate) {
+    .controller('SidemenuCtrl', function ($scope, authService, $ionicSideMenuDelegate, $ionicPopover) {
     // $scope.refreshing = false;
     $scope.logout = function () {
         $ionicSideMenuDelegate.toggleLeft();
         console.log("logging out");
         authService.logout();
     };
+
+    $ionicPopover.fromTemplateUrl('components/sidemenu/popover.html', {
+        scope: $scope,
+      }).then(function(popover) {
+        $scope.popover = popover;
+      });
+
+      $scope.clicker = function(){
+          console.log("clicked");
+      }
 
     // $scope.refresh = function () {
     //     console.log("refreshing");
